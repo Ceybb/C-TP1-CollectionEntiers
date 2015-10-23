361c5ecb339ad54a4611c1463c89e8da28062b6b
Ceybb <Ceybb@users.noreply.github.com>
2015-10-23 02:50:25 +0200

    Update Test.cpp
    Ajout ds test liés aux collections vides / paramètres nuls

--- Test.cpp
+++ Test.cpp
@@ -24,6 +24,12 @@ void TestConstructeurAfficher1 ()
 	collection.Afficher();
 }
 
+void TestConstructeurAfficher2 ()
+{
+	CollectionEntiers collection (0);
+	collection.Afficher();
+}
+
 void TestConstructeurDeuxAfficher1 ()
 {
 	int tab []={13, 28, 47, 59, 71, 1062, 0, 12};
@@ -32,6 +38,13 @@ void TestConstructeurDeuxAfficher1 ()
 }
 
 void TestConstructeurDeuxAfficher2 ()
+{
+	int tab []={};
+	CollectionEntiers collection(0,tab);
+	collection.Afficher();
+}
+
+void TestConstructeurDeuxAfficher3 ()
 {
 	int tab []={13, 28, 13, 59, 59, 1062, 0, 13, 20};
 	CollectionEntiers collection(9,tab);
@@ -49,6 +62,13 @@ void TestAjouter1 ()
 
 void TestAjouter2 ()
 {
+	CollectionEntiers collection (0);
+	collection.Ajouter(14);
+	collection.Afficher();
+}
+
+void TestAjouter3 ()
+{
 	CollectionEntiers collection (13);
 	collection.Ajouter(14);
 	collection.Ajouter(17);
@@ -59,7 +79,7 @@ void TestAjouter2 ()
 	collection.Afficher();
 }
 
-void TestAjouter3 ()
+void TestAjouter4 ()
 {
 	CollectionEntiers collection (4);
 	collection.Ajouter(14);
@@ -86,12 +106,43 @@ void TestRetirer1 ()
 	collection.Ajouter(114);
 	collection.Ajouter(218);
 	collection.Afficher();
-	int tab []={133, 78, -165};
-	collection.Retirer(3,tab);
+	int tab []={133, 78, -165, 246};
+	collection.Retirer(4,tab);
+	collection.Afficher();
+}
+void TestRetirer2 ()
+{
+	CollectionEntiers collection (0);
+	collection.Afficher();
+	int tab []={133, 78, -165, 246};
+	collection.Retirer(4,tab);
 	collection.Afficher();
 }
 
-void TestRetirer2 ()
+void TestRetirer3 ()
+{
+	CollectionEntiers collection (15);
+	collection.Ajouter(14);
+	collection.Ajouter(17);
+	collection.Ajouter(-165);
+	collection.Ajouter(17);
+	collection.Ajouter(218);
+	collection.Afficher();
+	int tab []={};
+	collection.Retirer(0,tab);
+	collection.Afficher();	
+}
+
+void TestRetirer4 ()
+{
+	CollectionEntiers collection (0);
+	collection.Afficher();
+	int tab []={};
+	collection.Retirer(0,tab);
+	collection.Afficher();
+}
+
+void TestRetirer5 ()
 {
 	CollectionEntiers collection (17);
 	collection.Ajouter(14);
@@ -128,6 +179,37 @@ void TestAjuster1 ()
 
 void TestAjuster2 ()
 {
+	CollectionEntiers collection (0);
+	int codeRetourAjuster;
+	codeRetourAjuster=collection.Ajuster(15);
+	collection.Afficher();
+	cout << codeRetourAjuster << endl;
+}
+
+void TestAjuster3 ()
+{
+	CollectionEntiers collection (17);
+	collection.Ajouter(71);
+	collection.Ajouter(44);
+	collection.Ajouter(-35);
+	collection.Ajouter(17);
+	int codeRetourAjuster;
+	codeRetourAjuster=collection.Ajuster(0);
+	collection.Afficher();
+	cout << codeRetourAjuster << endl;
+}
+
+void TestAjuster4 ()
+{
+	CollectionEntiers collection (0);
+	int codeRetourAjuster;
+	codeRetourAjuster=collection.Ajuster(0);
+	collection.Afficher();
+	cout << codeRetourAjuster << endl;
+}
+
+void TestAjuster5 ()
+{
 	CollectionEntiers collection (17);
 	collection.Ajouter(71);
 	collection.Ajouter(44);
@@ -158,6 +240,37 @@ void TestReunir1 ()
 
 void TestReunir2 ()
 {
+	CollectionEntiers collection (0);
+	CollectionEntiers collection2 (7);
+	collection2.Ajouter(41);
+	collection2.Ajouter(0);
+	collection2.Ajouter(12);
+	collection.Reunir(collection2);
+	collection.Afficher();
+}
+
+void TestReunir3 ()
+{
+	CollectionEntiers collection (10);
+	collection.Ajouter(13);
+	collection.Ajouter(17);
+	collection.Ajouter(76);
+	collection.Ajouter(42);
+	CollectionEntiers collection2 (0);
+	collection.Reunir(collection2);
+	collection.Afficher();
+}
+
+void TestReunir4 ()
+{
+	CollectionEntiers collection (0);
+	CollectionEntiers collection2 (0);
+	collection.Reunir(collection2);
+	collection.Afficher();
+}
+
+void TestReunir5 ()
+{
 	CollectionEntiers collection (14);
 	collection.Ajouter(11);
 	collection.Ajouter(24);
@@ -173,7 +286,7 @@ void TestReunir2 ()
 	collection.Afficher();
 }
 
-void TestReunir3 ()
+void TestReunir6 ()
 {
 	CollectionEntiers collection (6);
 	collection.Ajouter(11);
@@ -192,41 +305,53 @@ void TestReunir3 ()
 
 
 
-void TestConstructeurAfficher()
+void TestConstructeurAfficher ()
 {
 	TestConstructeurAfficher1();
+	TestConstructeurAfficher2();
 }
 
-void TestConstructeurDeuxAfficher()
+void TestConstructeurDeuxAfficher ()
 {
 	TestConstructeurDeuxAfficher1();
 	TestConstructeurDeuxAfficher2();
+	TestConstructeurDeuxAfficher3();
 }
 
-void TestAjouter()
+void TestAjouter ()
 {
 	TestAjouter1();  
 	TestAjouter2(); 
 	TestAjouter3();
+	TestAjouter4();
 }
 
-void TestRetirer()  
+void TestRetirer ()  
 {
 	TestRetirer1();
 	TestRetirer2(); 
+	TestRetirer3();
+	TestRetirer4();
+	TestRetirer5();
 }
 
-void TestAjuster() 
+void TestAjuster () 
 {
 	TestAjuster1();  
 	TestAjuster2();
+	TestAjuster3();
+	TestAjuster4();
+	TestAjuster5();
 }
 
-void TestReunir()
+void TestReunir ()
 {
 	TestReunir1();
 	TestReunir2();  
 	TestReunir3();
+	TestReunir4();
+	TestReunir5();  
+	TestReunir6();
 }
 
 
